b0092d6bb943993c6be2f6263161604ac48c8c89
Ivan Metzlar <ivan.metzlar@eelogic.nl>
2013-05-08 16:45:39 -0700

    Working refresh with rooms

--- django_rt/node/server.js
+++ django_rt/node/server.js
@@ -4,44 +4,51 @@ var redis = require('redis'),
 
 app.listen(8080);
 
+queues = {};
+
 function handler (req, res) {
     res.writeHead(404);
     res.end('Not found');
 }
 
-function poll(socket, queue_name, redis_client) {
+function poll(queue_name, redis_client) {
     redis_client.blpop(queue_name, 0, function(err, data){
         console.warn('Found data in queue: ' + queue_name + '\n    data: ' + data);
-        socket.emit(queue_name, data[1]);
+        io.sockets.in(queue_name).emit(queue_name, data[1])
     });
 }
 
 io.sockets.on('connection', function (socket) {
     console.warn('New connection');
     var queue_name = null;
-    var interval = null;
-    var redis_client = redis.createClient();
-    redis_client.on("error", function (err) {
-        console.log("Error " + err);
-    });
     socket.on('client', function (data){
         queue_name = data['queue_name'];
         console.warn('New client -> '+queue_name);
-        interval = setInterval(function () {
-            poll(socket, queue_name, redis_client);
-        }, 100);
+        socket.join(queue_name);
+        
+        if(!queues[queue_name]){
+            var redis_client = redis.createClient();
+            redis_client.on("error", function (err) {
+                console.log("Error " + err);
+            });
+            var interval = setInterval(function () {
+                poll(queue_name, redis_client);
+            }, 100);
+            queues[queue_name] = {
+                interval:interval,
+                redis_client:redis_client
+            };
+        }
     });
     socket.on('disconnect', function () {
         if(queue_name!=null){
             console.warn('Disconnected '+queue_name);
-        }
-        if(interval!=null){
-            clearInterval(interval);
-            interval = null;
-        }
-        if(redis_client!=null){
-            redis_client.end();
-            redis_client = null;
+            client_list = io.sockets.clients(queue_name);
+            if(client_list.length==0){
+                clearInterval(queueus[queue_name].interval);
+                queues[queue_name].redis_client.end();
+                queues[queue_name] = false;
+            }
         }
     });
 });
